c954e82fd423abe8c69ad5a58dc5fb0c8cb06d2d
ikozul00 <ivana.kozul5@gmail.com>
2021-11-03 11:17:44 +0100

    Add mobile header

--- modules/header/header.js
+++ modules/header/header.js
@@ -4,6 +4,12 @@ import LogoImg from '../../assets/logo.png';
 import HeroImg from '../../assets/hero.png';
 import NavBar from '../../components/navbar';
 
+const hamburgerWidth = 45;
+const hamburgerHeight = hamburgerWidth / (35 / 50);
+
+const magnifierWidth = 26;
+const magnifierHeight = magnifierWidth / (26 / 25);
+
 const Header = () => {
     return (
         <header className="h-80 relative flex items-center">
@@ -16,7 +22,7 @@ const Header = () => {
                 alt="Hero image"
             />
             <main className="max-w-4xl mx-auto flex-grow flex flex-col">
-                <div className="z-10 flex justify-between items-center mb-16">
+                <div className="z-10 flex justify-between items-center sm:mb-16">
                     <Image
                         src={LogoImg}
                         layout="fixed"
@@ -24,6 +30,25 @@ const Header = () => {
                         height={50}
                         alt="Design logo"
                     />
+                    <div className="flex items-center sm:hidden">
+                        <Image
+                            className="cursor-pointer"
+                            src={'/magnifier.svg'}
+                            layout="fixed"
+                            width={magnifierWidth}
+                            height={magnifierHeight}
+                            alt="Menu"
+                        />
+                        <div className="w-8" />
+                        <Image
+                            className="cursor-pointer"
+                            src={'/hamburger.svg'}
+                            layout="fixed"
+                            width={hamburgerWidth}
+                            height={hamburgerHeight}
+                            alt="Menu"
+                        />
+                    </div>
                     <NavBar />
                 </div>
                 <div className="z-10">
